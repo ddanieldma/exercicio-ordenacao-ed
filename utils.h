43656edc77d6e60f8abea812fdbd6389024f4e71
ddanieldma <danielalmemiranda@gmail.com>
2024-05-08 00:36:42 -0300

    função que troca valores dos nodes

--- utils.h
+++ utils.h
@@ -5,3 +5,9 @@ using namespace std;
 void iguais(){
     cout << "=====================================" << endl;
 }
+
+void swapNodesValues(int& iVal1, int& iVal2){
+	int iTemp = iVal1;
+	iVal1 = iVal2;
+	iVal2 = iTemp;
+}
