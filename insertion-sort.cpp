2a9a81e529f14b24fc81e379b6f291c8d71737ba
ddanieldma <danielalmemiranda@gmail.com>
2024-05-17 17:24:13 -0300

    insertion sort para array

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -2,32 +2,86 @@
 
 using namespace std;
 
+void iguais();
+bool invalidArray(int*);
 void displayArray(int*, int);
+void insertInArray(int*, int, int, int, int);
 void insertionSort(int*, int);
 
 
 int main(){
-    int iarr[] = {13, 1, 666, 42, 2};
-    int iArrSize = sizeof(iarr)/sizeof(iarr[0]);
+    int arri[] = {13, 1, 666, 42, 2};
+    int iSize = sizeof(arri)/sizeof(arri[0]);
     
-    displayArray(iarr, iArrSize);
+    displayArray(arri, iSize);
+    iguais();
 
+    insertionSort(arri, iSize);
+
+    displayArray(arri, iSize);
     return 0;
 }
 
 
-void displayArray(int* iarr, int iSize){
+void iguais(){
+    cout << "======================================" << endl;
+}
+
+bool invalidArray(int* arri){
+    if (arri = nullptr){
+        cout << "Array inválido" << endl;
+
+        return true;
+    }
+
+    return false;
+}
+
+void displayArray(int* arri, int iSize){
+    if (invalidArray(arri))
+        return;
+    
     cout << "Elementos do array: ";
     
     for (int i = 0; i < iSize; i++){
-        cout << iarr[i] << " ";
+        cout << arri[i] << " ";
     }
 
     cout << endl;
 }
 
-void insertionSort(int* , int iSize){
-    for (int outerLoop = 0; outerLoop < iSize; outerLoop++){
+void insertInArray(int* arri, int iSize, int iDestIndex, int iOrigIndex, int iValue){
+    // Insere um elemento no array no index especificado, movendo todos 
+    // os outro elementos para frente.
+    // 
+    
+    if(invalidArray(arri))
+        return;
 
+    for (int i = iOrigIndex; i > iDestIndex; i--){
+        // Move todos os elementos uma posição pra frente.
+        arri[i] = arri[i - 1];
     }
+
+    // E insere na posição específca o valor.
+    arri[iDestIndex] = iValue;
+
+    return;
+}
+
+void insertionSort(int* arri, int iSize){
+    // Ordena o array dado usando o algorítmo de Insertion Sort.
+    
+    if(arri == nullptr)
+        return;
+
+    for (int outerLoop = 0; outerLoop < iSize; outerLoop++){
+        for (int innerLoop = 0; innerLoop < outerLoop; innerLoop++){
+            if (arri[outerLoop] < arri[innerLoop]){
+                insertInArray(arri, iSize, innerLoop, outerLoop, arri[outerLoop]);
+            }
+        }
+    }
+
+    return;
 }
